7f2701bdfd5854c567bb917bcbc1b904aceed962
Jrootexe <137057219+Jrootexe@users.noreply.github.com>
2023-08-17 18:21:28 -0400

    Create array for results and fix output repetition

--- scripts.js
+++ scripts.js
@@ -14,37 +14,48 @@ function getComputerChoice(){
 
 // Create function to get results of playerSelection and computerSelection in the game
 
-function playRound(playerSelection, computerSelection){
+let record = []; 
+
+function playRound(playerSelection, computerSelection){ 
     if (playerSelection === "rock" && computerSelection === "paper") {
+        record.push("Lose");
         return("You Lose! Paper beats Rock!");
 
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        record.push("Lose");
         return("You Lose! Scissors beat Paper!");
 
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        record.push("Lose");
         return("You Lose! Rock beats Scissors!");
 
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        record.push("Win");
         return("You Win! Rock beats Scissors!");
 
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        record.push("Win");
         return("You Win! Paper beats Rock!");
 
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        record.push("Win");
         return("You Win! Scissors beat Paper!");
 
     } else if (playerSelection === "rock" && computerSelection === "rock") {
+        record.push("Draw");
         return("It's a draw! Rock can't beat Rock!");
 
     } else if (playerSelection === "paper" && computerSelection === "paper") {
+        record.push("Draw");
         return("It's a draw! Paper can't beat Paper!");
 
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        return("It's a draw! Scissors can't beat Scissors!")
+        record.push("Draw");
+        return("It's a draw! Scissors can't beat Scissors!");
     }
 }
 
-const computerSelection = getComputerChoice().toLowerCase();
+let computerSelection = getComputerChoice().toLowerCase();
 
 // playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
 
@@ -55,21 +66,33 @@ const computerSelection = getComputerChoice().toLowerCase();
 function game(){
     let playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
     
-    console.log(playRound(playerSelection, computerSelection));
-
-    playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
+    let computerSelection = getComputerChoice().toLowerCase();
 
     console.log(playRound(playerSelection, computerSelection));
 
     playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
+    
+    computerSelection = getComputerChoice().toLowerCase();
 
     console.log(playRound(playerSelection, computerSelection));
 
     playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
+    
+    computerSelection = getComputerChoice().toLowerCase();
 
     console.log(playRound(playerSelection, computerSelection));
 
     playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
+    
+    computerSelection = getComputerChoice().toLowerCase();
 
     console.log(playRound(playerSelection, computerSelection));
+
+    playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
+    
+    computerSelection = getComputerChoice().toLowerCase();
+
+    console.log(playRound(playerSelection, computerSelection));
+
+    return(record);
 }
