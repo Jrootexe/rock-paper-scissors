4d7cd0889e463c922923d423a26b327fa09dac12
Jrootexe <137057219+Jrootexe@users.noreply.github.com>
2023-09-19 19:23:52 -0400

    Have each result displayed on a new line

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-//create function to get random result from computer
 
 function getComputerChoice(){
     let x = Math.floor((Math.random() * 100) + 1);
@@ -12,7 +11,7 @@ function getComputerChoice(){
     }
 }
 
-// Create function to get results of playerSelection and computerSelection in the game
+//
 
 let record = [];
 
@@ -20,63 +19,59 @@ function playRound(playerSelection, computerSelection){
     
     if (playerSelection === "rock" && computerSelection === "paper") {
         record.push("Lose");
-        return("You Lose! Paper beats Rock!");
+        return("<p>You Lose! Paper beats Rock!");
 
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         record.push("Lose");
-        return("You Lose! Scissors beat Paper!");
+        return("<p>You Lose! Scissors beat Paper!");
 
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         record.push("Lose");
-        return("You Lose! Rock beats Scissors!");
+        return("<p>You Lose! Rock beats Scissors!");
 
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         record.push("Win");
-        return("You Win! Rock beats Scissors!");
+        return("<p>You Win! Rock beats Scissors!");
 
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         record.push("Win");
-        return("You Win! Paper beats Rock!");
+        return("<p>You Win! Paper beats Rock!");
 
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         record.push("Win");
-        return("You Win! Scissors beat Paper!");
+        return("<p>You Win! Scissors beat Paper!");
 
     } else if (playerSelection === "rock" && computerSelection === "rock") {
         record.push("Draw");
-        return("It's a draw! Rock can't beat Rock!");
+        return("<p>It's a draw! Rock can't beat Rock!");
 
     } else if (playerSelection === "paper" && computerSelection === "paper") {
         record.push("Draw");
-        return("It's a draw! Paper can't beat Paper!");
+        return("<p>It's a draw! Paper can't beat Paper!");
 
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
         record.push("Draw");
-        return("It's a draw! Scissors can't beat Scissors!");
+        return("<p>It's a draw! Scissors can't beat Scissors!");
     }
 }
 
 let computerSelection = getComputerChoice().toLowerCase();
 
-// playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
-
-// console.log(playRound(playerSelection, computerSelection));
-
-//create function to play five rounds of the game and determine who the winner is
-
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        alert(playRound(button.id, getComputerChoice().toLowerCase()));
+        document.getElementById('results').innerHTML += playRound(button.id, getComputerChoice().toLowerCase());
     });
 });
 
+// document.getElementById('results').innerHTML = playRound()
+
 function game(){
     record.splice(0, 5)
-
-    let playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
     
-    let computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
+    
+    computerSelection = getComputerChoice().toLowerCase();
 
     console.log(playRound(playerSelection, computerSelection));
 
